09995b11f2f7520d5a15c590f5832ceae3b34a7c
adhistia20 <adhistiaaisy@gmail.com>
2023-10-28 13:03:56 +0700

    profile

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,12 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Home;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/profile', [Home::class, 'profile']);
+
+$routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
+
